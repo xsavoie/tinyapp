b1536c8516f61b45b943fdd93704404374b9e91a
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-27 21:34:35 +0000

    Added body-parser and implemented random string function

--- express_server.js
+++ express_server.js
@@ -4,6 +4,9 @@ const PORT = 8080; //default port 8080
 
 app.set('view engine', 'ejs');
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -39,3 +42,14 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  return Math.random().toString(20).substr(2, 6)
+}
+
+console.log(generateRandomString())
