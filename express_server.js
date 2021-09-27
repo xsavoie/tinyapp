81585885501a4d7eb9d2a3f69f9b5d30c7964ad3
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-27 21:16:19 +0000

    Created urls_new view and updated express_server

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 
 const urlDatabase = {
@@ -10,30 +10,30 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
-
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-
 app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n')
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
+
+app.get('/urls/new', (req, res) =>{
+  res.render('urls_new');
+})
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
