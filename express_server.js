f149d42bf428361da25dafa1203c241a9bd9c4eb
Xavier Savoie <xaviersavoie@gmail.com>
2021-09-28 19:12:10 +0000

    Implemented edit link function

--- express_server.js
+++ express_server.js
@@ -7,7 +7,6 @@ app.set('view engine', 'ejs');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
@@ -51,7 +50,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-//post request to add new Url's
+// post request to add new Url's
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   let randomString = generateRandomString()
@@ -66,6 +65,18 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[shortURL]
   console.log(urlDatabase) // test log to see if Database updated
   res.redirect("/urls");
+});
+
+// post to update longURL
+app.post("/urls/:shortURL/update", (req, res) => {
+  const shortURL = req.params.shortURL
+  urlDatabase[shortURL] = req.body.newURL
+  res.redirect("/urls")
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL
+  res.redirect(`/urls/${shortURL}`)
 });
 
 app.get("/u/:shortURL", (req, res) => {
